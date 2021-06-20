2cfaad032f73733da96582f9e19ccb3f6989aeac
sahilshirodkar1630 <sahilshirodkar1630@gmail.com>
2021-06-21 03:13:40 +0530

    Day20

---
+++ Day20/CheckifArrayIsSortedAndRotated.java
@@ -0,0 +1,26 @@
+package Day20;
+
+public class CheckifArrayIsSortedAndRotated {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+public boolean check(int[] A) {
+        
+        int k = 0, n = A.length;
+        for (int i = 0; i < n; ++i) {
+            if (A[i] > A[(i + 1) % n]) {
+                k++;
+            }
+            if (k > 1) {
+                return false;
+            }
+        }
+        return true;
+      
+    }
+
+}
